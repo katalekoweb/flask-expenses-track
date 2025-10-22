05aedd3dba698c3dd6fae3ae01df143df1a03234
Kataleko <katalekoweb@gmail.com>
2025-10-22 18:29:12 +0100

    config database

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import  Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    expenses = [
+        {'item': 'Coffee', 'amount': 3.5},
+        {'item': 'Books', 'amount': 12.99},
+        {'item': 'Groceries', 'amount': 45.23}
+    ]
+    
+    return render_template('index.html', expenses=expenses)
 
 
 if __name__ == '__main__':
