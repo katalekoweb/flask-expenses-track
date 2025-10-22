09ddde674d215809a0dafb599e1fe3be54ed45cd
Kataleko <katalekoweb@gmail.com>
2025-10-22 18:34:02 +0100

    feat: create model and init the database

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+db = SQLAlchemy(app)
+
+class Expenses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(200))
+    date = db.Column(db.DateTime, nullable=False)
 
 @app.route('/')
 def index():
@@ -16,4 +24,6 @@ def index():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=2030)
